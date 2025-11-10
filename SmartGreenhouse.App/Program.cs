51af766eaebe1bcb94b623c2d807d31378f5761f
Polina Baranova <emailforeducation1@gmail.com>
2025-11-10 07:59:40 +0200

    Preparing project  for Vagrant

---
+++ SmartGreenhouse.App/Program.cs
@@ -0,0 +1,11 @@
+﻿using SmartGreenhouse.Services;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var simulator = new SimulationService();
+        Console.WriteLine("Симуляція разумної теплиці запущена!");
+    }
+}
+
