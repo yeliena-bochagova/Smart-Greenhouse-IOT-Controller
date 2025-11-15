6607fe1b1d27bfefec246f1208c5687731a05c8b
Polina Baranova <emailforeducation1@gmail.com>
2025-11-15 22:08:30 +0200

    minor changes to files

--- SmartGreenhouse.Tests/obj/Debug/net9.0/SmartGreenhouse.Tests.AssemblyInfo.cs
+++ SmartGreenhouse.Tests/obj/Debug/net9.0/SmartGreenhouse.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartGreenhouse.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51af766eaebe1bcb94b623c2d807d31378f5761f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a649e0ee1cd5425e2ca236140117cddf1cd49245")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartGreenhouse.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartGreenhouse.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
